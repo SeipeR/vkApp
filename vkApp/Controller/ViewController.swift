a00a418d6b8c5f35345507d949b53e6d92a57924
SeipeR <se1per@yandex.ru>
2021-04-08 17:04:56 +0300

    Организована структура приложения

--- vkApp/Controller/ViewController.swift
+++ vkApp/Controller/ViewController.swift
@@ -12,11 +12,6 @@ class ViewController: UIViewController {
     @IBOutlet weak var logintTextfield: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBAction func buttonTapped(_ sender: Any) {
-        if (logintTextfield.text == "admin") && (passwordTextField.text == "admin") {
-            print("Осуществлён вход с учётной записи администратора!")
-        } else {
-            print("В доступе отказано!")
-        }
     }
     @IBAction func unwindSegue(for unwindSegue: UIStoryboardSegue) {
     }
