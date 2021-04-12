f59216a922fb2e2be9c3f6144846259c3c4cdd5d
SeipeR <se1per@yandex.ru>
2021-04-12 15:19:08 +0300

    Реализован CollectionView

---
+++ vkApp/View/Reusable/PhotoCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoCell.swift
+//  vkApp
+//
+//  Created by Дамир Доронкин on 12.04.2021.
+//
+
+import UIKit
+
+class PhotoCell: UICollectionViewCell {
+    func configurate() {
+        //Configurate cell
+    }
+}
