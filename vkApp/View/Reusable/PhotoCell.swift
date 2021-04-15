42bec791adc5bce4380bcd8e315a4358699294ef
SeipeR <se1per@yandex.ru>
2021-04-15 17:49:04 +0300

    Реализована возможность постановки лайков

--- vkApp/View/Reusable/PhotoCell.swift
+++ vkApp/View/Reusable/PhotoCell.swift
@@ -12,15 +12,22 @@ class PhotoCell: UICollectionViewCell {
     @IBOutlet weak var photo: UIImageView!
     @IBOutlet weak var likeCountLabel: UILabel!
     @IBOutlet weak var likeButtonOutlet: UIButton!
-    @IBAction func likeButtonAction(_ sender: Any) {
+    @IBAction func action(_ sender: UIButton) {
+        if sender.image(for: .normal) == UIImage(systemName: "heart.fill") {
+            sender.setImage(UIImage(systemName: "heart"), for: .normal)
+            likeCountLabel.text = "\(UInt32(likeCountLabel.text!)! - 1)"
+        } else {
+            sender.setImage(UIImage(systemName: "heart.fill"), for: .normal)
+            likeCountLabel.text = "\(UInt32(likeCountLabel.text!)! + 1)"
+        }
     }
+    
     
     func configure(image: UIImage?, isLiked: Bool, likeCount: UInt32) {
         photo.image = image
         likeCountLabel.text = "\(likeCount)"
         if isLiked {
             likeButtonOutlet.setImage(UIImage(systemName: "heart.fill"), for: .normal)
-            likeCountLabel.text = "\(likeCount + 1)"
         }
     }
 }
