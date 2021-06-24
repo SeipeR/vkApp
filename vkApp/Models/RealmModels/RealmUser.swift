8127fd3442ee4823d5f7644176a5c4c039b7dfd2
SeipeR <se1per@yandex.ru>
2021-06-24 19:45:30 +0300

    Revert "Preparing for data output"
    This reverts commit 2a9f27f75b86470d78b55d695db18dba0994f1df.

--- vkApp/Models/RealmModels/RealmUser.swift
+++ vkApp/Models/RealmModels/RealmUser.swift
@@ -5,8 +5,4 @@ class RealmUser: Object {
     @objc dynamic var firstName: String = ""
     @objc dynamic var lastName: String = ""
     @objc dynamic var userAvatarURL: String = ""
-    
-    override class func primaryKey() -> String? {
-        "id"
-    }
 }
