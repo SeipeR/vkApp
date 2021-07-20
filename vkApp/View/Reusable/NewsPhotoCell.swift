9b67961c7138e2d14d251e0c3d822f90c72a5913
SeipeR <se1per@yandex.ru>
2021-07-20 11:37:31 +0300

    Added dynamic resizing of images

--- vkApp/View/Reusable/NewsPhotoCell.swift
+++ vkApp/View/Reusable/NewsPhotoCell.swift
@@ -1,5 +1,22 @@
 import UIKit
 
+class ScaledHeightImageView: UIImageView {
+    override var intrinsicContentSize: CGSize {
+        if let myImage = self.image {
+            let myImageWidth = myImage.size.width
+            let myImageHeight = myImage.size.height
+            let myViewWidth = self.frame.size.width
+            
+            let ratio = myViewWidth / myImageWidth
+            let scaledHeight = myImageHeight * ratio
+            
+            return CGSize(width: myViewWidth, height: scaledHeight)
+        }
+        
+        return CGSize(width: -1.0, height: -1.0)
+    }
+}
+
 class NewsPhotoCell: UITableViewCell {
     @IBOutlet weak var newsImage: UIImageView!
     
